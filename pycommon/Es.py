63807faf1a67df1de3ea56fc2d805db7f093eff9
yangyahe <yangyahe@mafengwo.com>
2022-02-20 18:07:27 +0800

    增加 es的更新方法

--- pycommon/Es.py
+++ pycommon/Es.py
@@ -1,6 +1,6 @@
 import json
-import datetime
 from elasticsearch import Elasticsearch, helpers
+
 
 class Es():
   """ES类
@@ -16,13 +16,14 @@ class Es():
   :index_name(str): 索引名
   :type_name(str): 类型名
   """
+
   def __init__(self, host_key, index_name, type_name):
     self.index_name = index_name
     self.type_name = type_name
     # 在进行任何操作前先进行嗅探，节点没有响应时刷新重新连接，每60秒刷新一次
     self.es = Elasticsearch(host_key, timeout=60, sniff_on_start=True,
-                        sniff_on_connection_fail=True, sniffer_timeout=60)
-  
+                            sniff_on_connection_fail=True, sniffer_timeout=60)
+
   def getEsData(self, body, *returnFields):
     """获取ES
     用scroll的方式获取一个index-type的大量数据，未考虑内存控制
@@ -36,10 +37,10 @@ class Es():
     assert "query" in body, u"body 中必须含有 query 这个查询体,body: {a}".format(
         a=json.dumps(body))
     res = self.es.search(index=self.index_name, doc_type=self.type_name,
-                     body=body, scroll="1m")
+                         body=body, scroll="1m")
     total_num = res["hits"]["total"]
     now_num = 0
-    
+
     docId_list = []
     doc_dict = dict()
     returnField_dict = dict()
@@ -49,7 +50,8 @@ class Es():
         return docId_list, doc_dict, returnField_dict
 
     while True:
-        print("now process {}----".format(100.0 * now_num / total_num))
+        print("正在查询索引: {}, 进度: {}----".format(self.index_name,
+              100.0 * now_num / total_num))
         assert res["timed_out"] == False, "已经超时，本次传输基本失败"
         __scroll_id = res["_scroll_id"]
         for item_doc in res["hits"]["hits"]:
@@ -72,3 +74,44 @@ class Es():
     )
     return docId_list, doc_dict, returnField_dict
 
+  def updateEsData(self, id_doc_dict):
+    """更新ES
+    既能更新，也能新建不存在的doc和字段
+
+    Args:
+        id_doc_dict(dict), key为要更新的es的id，value也是一个字典（key为要更新的字段名，value为要更新的值）
+    """
+    if len(id_doc_dict) == 0:
+        print('要更新的实时数据为空')
+        return
+    assert len(id_doc_dict) <= 2000, "更新太多了，es承受不住"
+
+    tag_doc_ = []
+    print("正在更新es……", self.index_name)
+    for id, doc in id_doc_dict.items():
+      assert isinstance(doc, dict), u"doc 必须是一个 dict类型"
+      _id = id
+
+      newItem = dict()
+      for key, value in doc.items():  # 同时取出键和值
+        newItem[key] = value
+
+      chunk_all = dict()
+      chunk_all["doc"] = newItem
+      chunk_all["doc_as_upsert"] = "true"
+      chunk_all["_index"] = self.index_name
+      chunk_all["_type"] = self.type_name
+      chunk_all["_id"] = _id
+      chunk_all["_op_type"] = "update"
+
+      tag_doc_.append(chunk_all)
+
+    sucess_num, error_list = helpers.bulk(
+        self.es, tag_doc_)  # (client  doc集合list)
+    if sucess_num != len(id_doc_dict):
+      print("验证文件行必须和ES成功bulk数目必须一致，现在不是")
+    if error_list:
+      process_msg = "build error {ids}".format(ids=str(error_list))
+      print(process_msg)
+
+    print("本次更新目标文档数: " + str(sucess_num))
